07ed147fb68d8126e74b2f3f3e91bf93d7e1ad13
Shalin <shalinmodi60@gmail.com>
2025-07-15 11:54:39 +0530

    second commit

--- core/urls.py
+++ core/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('respond-request/<int:req_id>/', views.respond_request_view, name='respond_request'),
     path('chat/<int:req_id>/', views.chat_view, name='chat'),
+    path('chat-ajax/<int:req_id>/', views.chat_ajax_view, name='chat_ajax'),
     path('requests/sent/', views.requests_sent_view, name='requests_sent'),
     path('requests/received/', views.requests_received_view, name='requests_received'),
     path('chat-list/', views.chat_list_view, name='chat_list'),
+    path('user-profile/<int:user_id>/', views.user_profile_view, name='user_profile'),
 ]
