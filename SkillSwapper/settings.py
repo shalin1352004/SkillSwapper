30cc04dd66205be500a78d66234e86ba31376585
Shalin <shalinmodi60@gmail.com>
2025-07-16 12:06:22 +0530

    fourth commit

--- SkillSwapper/settings.py
+++ SkillSwapper/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+DJANGO_SECRET_KEY="z#)tcice$uj0)y3kxl&na+ns-2zq+=+--j^(g6z=$j@-bcna41"
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-insecure-key')
 
